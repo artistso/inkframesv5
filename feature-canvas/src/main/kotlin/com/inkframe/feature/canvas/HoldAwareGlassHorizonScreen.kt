937b4f92ffcf23715719c9a752e499a901dd9b11
Steven Owens <Soquarky@artistso.com>
2026-07-19 03:25:47 -0700

    Fix Brush Lab wrapper Compose import

--- feature-canvas/src/main/kotlin/com/inkframe/feature/canvas/HoldAwareGlassHorizonScreen.kt
+++ feature-canvas/src/main/kotlin/com/inkframe/feature/canvas/HoldAwareGlassHorizonScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.shape.RoundedCornerShape
